e2c84cf83ecee561e728be039a293b36d5cef5b1
Alex Bieliakov <bieliakov.alex@gmail.com>
2019-03-15 16:00:00 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,26 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0bff60fb779498e69ea705825a2ca1a5497a4fccef93bf3275705c2d27528f2f" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
+ '(package-selected-packages
+   (quote
+    (highlight-indent-guides ivy-yasnippet yasnippet-snippets yasnippet company-irony-c-headers company-irony company-quickhelp company paredit iedit magit counsel org-bullets cycle-themes dracula-theme exotica-theme ido-grid-mode ido-describe-bindings))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
