87bd57fb47a807ecff2a23b1bf7b0b0b830b4bc0
Alex Bieliakov <bieliakov.alex@gmail.com>
2019-03-20 19:21:36 +0200

    Another minor changes

--- init.el
+++ init.el
@@ -20,20 +20,4 @@ package-enable-at-startup nil)
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
- '(package-selected-packages
-   (quote
-    (highlight-indent-guides ivy-yasnippet yasnippet company-irony-c-headers company-irony company-quickhelp company irony paredit iedit magit counsel org-bullets cycle-themes dracula-theme use-package ido-grid-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(show-paren-match ((t (:underline "green")))))
+
