7d833afb4990a5d25aa9ea566d1188601eaf67dc
Alex Bieliakov <bieliakov.alex@gmail.com>
2019-03-25 18:37:48 +0200

    Temporary added hel

--- init.el
+++ init.el
@@ -21,3 +21,20 @@
 
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
+ '(package-selected-packages
+   (quote
+    (helm-projectile helm highlight-indent-guides ivy-yasnippet yasnippet company-irony-c-headers company-irony company-quickhelp company irony paredit iedit magit counsel org-bullets cycle-themes ido-grid-mode dracula-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-match ((t (:underline "green")))))
